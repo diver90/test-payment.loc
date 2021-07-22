6f5ca93dd98745eaeb1b3ab306259633c7ddf067
diver90 <mihail.zavalniuk@gmail.com>
2021-07-22 15:14:17 +0300

    improve ApiController callback function

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Client\Response;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -12,18 +10,19 @@ class ApiController extends Controller
     public function callback(Request $request)
     {
         $data = $request->all();
-        if(!empty($data)){
+        if (!empty($data)) {
             $order = Order::find($data['order']['order_id']);
-            $order->status = $data['order']['status'];
-            $order->save();
-
-            if($data['transaction']['status'] === 'fail'){
-                $fail = $data['transaction'];
-                $fail['message'] = $data['error']['recommended_message_for_user'];
-                session()->flash('fail', $fail);
-                return \response()->json('/sorry');
-            } elseif ($data['transaction']['status'] === 'success') {
-                return \response()->json('/thank-you');
+            if (!empty($order)) {
+                $order->status = $data['order']['status'];
+                $order->save();
+                if ($data['transaction']['status'] === 'fail') {
+                    $fail = $data['transaction'];
+                    $fail['message'] = $data['error']['recommended_message_for_user'];
+                    session()->flash('fail', $fail);
+                    return \response()->json('/sorry');
+                } elseif ($data['transaction']['status'] === 'success') {
+                    return \response()->json('/thank-you');
+                }
             }
         }
         return \response()->json('/');
